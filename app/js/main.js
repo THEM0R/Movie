5b5713e240da417e35fa75f0e83b166e4eccd807
TheMor <the.mor.ua@gmail.com>
2019-06-20 17:15:15 +0300

    0.0.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,12 @@
 /* requires */
-////=require libs/jquery.min.js // 1.8.3
 //=require libs/jquery-3.4.1.min.js // 3.4.1
 //=require libs/popper.min.js
+//=require libs/bootstrap.min.js
 //=require libs/owl.carousel.min.js
-// bootstrap
-//=require libs/bootstrap-4.3.1/util.js
-//=require libs/bootstrap-4.3.1/collapse.js
-////
-// =require libs/bootstrap-4.3.1/carousel.js
-//// =require libs/bootstrap-4.3.1/tooltip.js
 
-////=require libs/bootstrap-4.3.1/scrollspy.js
-////=require libs/bootstrap-4.3.1/alert.js
-////=require libs/bootstrap-4.3.1/button.js
-////=require libs/bootstrap-4.3.1/dropdown.js
-////=require libs/bootstrap-4.3.1/modal.js
-////=require libs/bootstrap-4.3.1/popover.js
-////=require libs/bootstrap-4.3.1/tab.js
-////=require libs/bootstrap-4.3.1/toast.js
 
 // js
 //=require js/owl.carousel.js
-//=require js/article.js
-//=require js/speed.search.js
 
 
 
